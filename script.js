f8457f0d62cbbb25e079bab0d7153fe209354e38
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-03-31 19:17:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const reverseSeq = n => {
   }
   return arr;
 };
+
+//Write a function that takes an array of numbers and returns the sum of the numbers. 
+//The numbers can be negative or non-integer. If the array does not contain any numbers 
+//then you should return 0.
+
+function sum (numbers) {
+  return numbers.reduce((a,b) => a + b, 0);
+};
