7a0043d214d31597b105d7a707b7a1b41b6fe9f9
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-04-02 02:08:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ function sum (numbers) {
 function simpleMultiplication(number){
   return number % 2 === 0 ? number * 8 : number * 9
 } 
+
+//Volume (centimeters cubed) of a cuboid formula:
+//Volume = Length * Width * Height
+//We only have the volume, and the length of a single side
+//Find out whether the cuboid has equal sides (= is a cube).
+//Return true if the cuboid could have equal sides, return false otherwise.
+//Return false for invalid numbers too (e.g volume or side is less than or equal to 0).
+//Note: side will be an integer
+
+var cubeChecker = function(volume, side){
+  if (volume <= 0 || side <= 0){
+    return false;
+  } else if (volume === side ** 3){
+      return true;
+  } else {
+    return false;
+  }
+}
