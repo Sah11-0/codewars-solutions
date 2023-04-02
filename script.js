31ff5fe3bf927e5b65b5f473e33e1f3f532c5f79
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-04-01 21:12:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,9 @@ const reverseSeq = n => {
 function sum (numbers) {
   return numbers.reduce((a,b) => a + b, 0);
 };
+
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number){
+  return number % 2 === 0 ? number * 8 : number * 9
+} 
