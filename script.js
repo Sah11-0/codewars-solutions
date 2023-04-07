7a979757b042fa6530567b9269ac3c8c6e651de3
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-04-07 17:22:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -129,3 +129,14 @@ function getStatus(isBusy) {
 function find(a, e) {
   return a.includes(e) ? a.indexOf(e) : "Not found"
 }
+
+//Complete the square sum function so that it squares each number passed into it and 
+//then sums the results together.
+
+function squareSum(numbers){
+  let sum = 0 
+  for(let i = 0; i < numbers.length; i++){
+    sum += (numbers[i] ** 2)
+  }
+  return sum;
+}
