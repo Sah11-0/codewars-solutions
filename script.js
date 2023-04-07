47d7b3f84cdf35961394acc7bcdb73048ad9330c
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-04-07 17:12:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -85,3 +85,18 @@ function multiply(a, b){
   let product = (a * b);
   return product;
 }
+
+//Consider an array/list of sheep where some sheep may be missing
+//from their place. We need a function that counts the number of sheep 
+//present in the array (true means present). Hint: Don't forget to check 
+//for bad values like null/undefined
+
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  for(let i = 0; i < arrayOfSheep.length; i++){
+    if (arrayOfSheep[i] === true){
+      count++
+    }
+  }
+  return count;
+}
