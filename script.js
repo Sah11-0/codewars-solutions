9a7c421e19433e9e308716bfd8c054e012f55b19
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-04-12 21:40:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -174,3 +174,15 @@ function otherAngle(a, b) {
   let c = 180 - (a + b) 
   return c;
 }
+
+//Let's play! You have to return which player won! In case of a draw return Draw!
+
+const rps = (p1, p2) => {
+  return p1 === p2 
+  ? "Draw!"
+  : ((p1 === "rock" && p2 === "scissors") || 
+     (p1 === "paper" && p2 === "rock") ||
+     (p1 === "scissors" && p2 === "paper")) 
+  ? "Player 1 won!"
+  : "Player 2 won!";
+}
