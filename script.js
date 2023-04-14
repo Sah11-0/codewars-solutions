e68c2a642c725765df66fdb769b9fbc45a163be6
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-04-14 14:32:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -212,3 +212,10 @@ function ifChuckSaysSo(){
 function stringToArray(string){
   return string.split(" ");
 }
+
+//ES6 has introduced another way of joining strings. Your task is to find out what this is 
+//and write a function that will add two strings together, there must be a space between the two strings.
+
+function joinStrings(string1, string2){
+  return `${string1} ${string2}`;
+}
