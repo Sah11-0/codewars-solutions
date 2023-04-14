4299875f63b0e909eebf772e7e2f8225230c97a5
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-04-14 14:31:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -206,3 +206,9 @@ function positiveSum(arr) {
 function ifChuckSaysSo(){
   return !true;
 }
+
+//Write a function to split a string and convert it into an array of words.
+
+function stringToArray(string){
+  return string.split(" ");
+}
