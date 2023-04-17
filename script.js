a8d08d40db573218614f42260ca41f11446a4265
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-04-17 16:00:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -270,3 +270,15 @@ function whatday(num) {
 let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     return days[num - 1] || "Wrong, please enter a number between 1 and 7"
 }
+
+//Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+function createArray(number){
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number; counter ++){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
