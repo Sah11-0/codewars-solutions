2a47f8e7f372d07c3c37ad6e13e3d631fbadc140
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-04-19 14:15:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -319,3 +319,20 @@ function makeNegative(num) {
 function makeNegative(num) {
   return -Math.abs(num)
 }
+
+//Your function takes two arguments:
+//current father's age (years)
+//current age of his son (years)
+//Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). 
+//The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - (sonYearsOld * 2))
+}
+//or 
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  if (dadYearsOld > (sonYearsOld * 2)){
+  return dadYearsOld - (sonYearsOld * 2)
+} else if (dadYearsOld < (sonYearsOld * 2)){
+} return (sonYearsOld * 2) - dadYearsOld 
+}
