8cef41203e6352def2141c2789dc9030f2bced40
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-04-19 14:13:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -305,3 +305,17 @@ function makeUpperCase(str) {
 }
 //or
 const makeUpperCase = str => str.toUpperCase()
+
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+function makeNegative(num) {
+  if (Math.sign(num) === 1){
+    return - + num
+  } else {
+    return num
+  }
+}
+//or
+function makeNegative(num) {
+  return -Math.abs(num)
+}
