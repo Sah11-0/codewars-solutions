3d6e3ad6112c11e639ce3a4be0fd3dcdb446b81d
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-04-28 23:19:36 -0400

    Create 8kyuAbbreviateName.js

---
+++ 8kyuAbbreviateName.js
@@ -0,0 +1,8 @@
+//Write a function to convert a name into initials. This kata strictly takes two words with one space in between them. The output should be two capital letters with a dot separating them.
+
+function abbrevName(name){
+    const words = name.split(" ")
+    const initials = words.map(word => word[0].toUpperCase())
+    const initialsWithDot = initials.join(".")
+    return initialsWithDot;
+  }
