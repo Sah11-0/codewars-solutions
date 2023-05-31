865eacfa96154224852bf9a8ed692bd185a1fafb
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-05-31 00:00:14 -0400

    Update 8kyuIsItAPalindrome.js

--- 8kyuIsItAPalindrome.js
+++ 8kyuIsItAPalindrome.js
@@ -1,6 +1,5 @@
 //Write a function that checks if a given string (case insensitive) is a palindrome. 
 
 function isPalindrome(x) {
-    const formattedStr = x.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
-    return formattedStr === formattedStr.split('').reverse().join('')
+    return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false
   }
