49bcee492d4353432393713dde3bc04711b95ea0
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-06-21 00:01:44 -0400

    Create 7kyuDisemvowelTrolls.js

---
+++ 7kyuDisemvowelTrolls.js
@@ -0,0 +1,11 @@
+//Your task is to write a function that takes a string and return a new string with all vowels removed. For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!". Note: for this kata y isn't considered a vowel.
+
+function disemvowel(str) {
+    const vowels = ['a','e','i','o','u','A','E','I','O','U'];
+    const chars = str.split('');
+    const filteredChars = chars.filter(function(char) {
+      return !vowels.includes(char);
+    });
+    const result = filteredChars.join('');
+    return result;
+  };
