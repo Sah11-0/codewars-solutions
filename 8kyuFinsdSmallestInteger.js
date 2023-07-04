164b6f5f003e5a1c88df06cd453da8d572ff875b
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-07-04 00:13:14 -0400

    Create 8kyuFinsdSmallestInteger.js

---
+++ 8kyuFinsdSmallestInteger.js
@@ -0,0 +1,14 @@
+//Given an array of integers your solution should find the smallest integer.
+//You can assume, for the purpose of this kata, that the supplied array will not be empty
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      let smallest = args[0];
+      for (let i = 1; i < args.length; i++) {
+        if (args[i] < smallest) {
+          smallest = args[i];
+        }
+      }
+      return smallest;
+    }
+  }
