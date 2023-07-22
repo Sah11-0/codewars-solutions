0bb595cfa5453050c9208482b0519cb842433c44
Sah11-0 <125934632+Sah11-0@users.noreply.github.com>
2023-07-21 21:00:53 -0400

    Update WhoAteTheCookie.js

--- WhoAteTheCookie.js
+++ WhoAteTheCookie.js
@@ -9,3 +9,9 @@ function cookie(x){
       return "Who ate the last cookie? It was the dog!"
     }
   }
+
+  //or
+
+  function cookie(x){
+    return typeof x == "string" ? "Who ate the last cookie? It was Zach!" : typeof x == "number" ? "Who ate the last cookie? It was Monica!" : "Who ate the last cookie? It was the dog!";
+  }
