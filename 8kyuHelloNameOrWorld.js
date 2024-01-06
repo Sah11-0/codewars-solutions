8668ca8798e3aeef0fa3b124e61b48b8c4b88a0f
Saraha <sah110_@outlook.com>
2024-01-05 22:13:35 -0500

    Create 8kyuHelloNameOrWorld.js

---
+++ 8kyuHelloNameOrWorld.js
@@ -0,0 +1,16 @@
+//Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String). Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+function hello(name) {
+    if (name && typeof name === 'string' && name.trim() !== '') {
+       const formattedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+       return `Hello, ${formattedName}!`;
+     } else {
+           return "Hello, World!";
+     }
+   }
+ 
+   //or
+
+function hello(name) {
+    return `Hello, ${name ? name[0].toUpperCase() + name.slice(1).toLowerCase() : "World"}!`
+  }
