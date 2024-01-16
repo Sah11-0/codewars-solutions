c542484acb2df34f26c03711fdc39ef078047264
Saraha <sah110_@outlook.com>
2024-01-15 19:32:54 -0500

    Update 8kyuLeoOscar.js

--- 8kyuLeoOscar.js
+++ 8kyuLeoOscar.js
@@ -15,4 +15,13 @@ function leo(oscar) {
       return "Leo got one already!"
     }
      }
+
+     //or
+
+     const leo = (oscar) => {
+      return oscar === 88 ? 'Leo finally won the oscar! Leo is happy' :
+             oscar === 86 ? 'Not even for Wolf of wallstreet?!'       :
+             oscar  <  88 ? 'When will you give Leo an Oscar?'        :
+             'Leo got one already!';
+    };
    
