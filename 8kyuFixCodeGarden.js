8cee63978940e724f43e93c3939d10d7f9dcebed
Saraha <sah110_@outlook.com>
2024-01-19 21:30:55 -0500

    Update 8kyuFixCodeGarden.js
    Refactor/add additional solution

--- 8kyuFixCodeGarden.js
+++ 8kyuFixCodeGarden.js
@@ -18,3 +18,10 @@ function rainAmount(mm){
          return "Your plant has had more than enough water for today!";
     }
 }
+
+//or
+
+function rainAmount(mm){
+     return mm < 40 ? `You need to give your plant ${40 - mm}mm of water` : "Your plant has had more than enough water for today!"
+ }
+ 
