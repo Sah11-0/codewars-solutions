8fe47b589a1f775c2bb4a94aaf8a91822ab83ba0
Saraha <sah110_@outlook.com>
2024-01-24 20:48:22 -0500

    Update 8kyuQuarterOfTheYear.js

--- 8kyuQuarterOfTheYear.js
+++ 8kyuQuarterOfTheYear.js
@@ -25,3 +25,26 @@ const quarterOf = (month) => {
   //or
 
   const quarterOf = month => Math.ceil(month / 3);
+
+  //or
+
+  const quarterOf = (month, quarter = 4) => {
+    switch (month) {
+      case 1:
+      case 2:
+      case 3:
+        quarter = 1;
+        break;
+      case 4:
+      case 5:
+      case 6:
+        quarter = 2;
+        break;
+      case 7:
+      case 8:
+      case 9:
+        quarter = 3;
+        break;
+    }
+    return quarter;
+  };
