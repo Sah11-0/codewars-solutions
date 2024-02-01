34fea5b6ce6e8cb5623315d3f158af71e0aa0aa1
Saraha <sah110_@outlook.com>
2024-01-31 21:49:49 -0500

    Create 8kyuSumTheStrings.js

---
+++ 8kyuSumTheStrings.js
@@ -0,0 +1,4 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+// If either input is an empty string, consider it as zero.
+
+sumStr = (a,b) => String(+a + +b);
