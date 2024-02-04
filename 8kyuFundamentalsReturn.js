02ef1c8af1ed411e89c4c7c30d27912b08ff3a32
Saraha <sah110_@outlook.com>
2024-02-04 09:03:34 -0500

    Update 8kyuFundamentalsReturn.js

--- 8kyuFundamentalsReturn.js
+++ 8kyuFundamentalsReturn.js
@@ -40,3 +40,12 @@ function exponent(a,b){
 function subt(a,b){
     return a - b;
 }
+
+//or
+
+const add = (a, b) => a + b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+const mod = (a, b) => a % b;
+const exponent = (a, b) => a ** b;
+const subt = (a, b) => a - b;
