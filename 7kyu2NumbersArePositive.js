2370d6fdd8a45d704e5374cbe1a22658ebf699bd
Saraha <sah110_@outlook.com>
2024-02-08 19:05:20 -0500

    Update 7kyu2NumbersArePositive.js

--- 7kyu2NumbersArePositive.js
+++ 7kyu2NumbersArePositive.js
@@ -1,7 +1,7 @@
 // Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of of the three integers are positive numbers (greater than zero), and False - otherwise.
 
 function twoArePositive(a, b, c) {
-    let positiveCount = 0
+    let positiveCount = 0;
   
     if (a > 0) {
       positiveCount++;
