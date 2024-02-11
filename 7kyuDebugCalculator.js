9a74e728ccbb2ad672ea66eb877d3ab01dcdd329
Saraha <sah110_@outlook.com>
2024-02-10 20:05:24 -0500

    Create 7kyuDebugCalculator.js

---
+++ 7kyuDebugCalculator.js
@@ -0,0 +1,19 @@
+//Debug a function called calculate that takes 3 values. The first and third values are numbers. The second value is a character. If the character is "+" , "-", "*", or "/", the function will return the result of the corresponding mathematical function on the two numbers. If the string is not one of the specified characters, the function should return null.
+
+var calculate = function calculate(a, o, b) {
+    var result;
+  
+    if (o === "+") {
+      return a + b;
+    } else if (o === "-") {
+      return a - b;
+    } else if (o === "/" && b !== 0) {
+      return a / b;
+    } else if (o === "*") {
+      return a * b;
+    } else {
+      result = null;
+    }
+  
+    return result;
+  };
