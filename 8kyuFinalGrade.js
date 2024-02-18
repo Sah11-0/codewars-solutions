95e143449ee7aabafbb6806c6c4ed1457337dc51
Saraha <sah110_@outlook.com>
2024-02-18 12:22:55 -0500

    Update 8kyuFinalGrade.js

--- 8kyuFinalGrade.js
+++ 8kyuFinalGrade.js
@@ -15,3 +15,5 @@ function finalGrade (exam, projects) {
            exam > 50 && projects >= 2 ? 75 : 0;
     }
   
+
+const finalGrade =  (exam, projects) => exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0;
