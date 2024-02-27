a1706803e950dd519c2f3a82116896597e6fdc94
Saraha <sah110_@outlook.com>
2024-02-26 21:10:51 -0500

    Create 8kyuFlickSwitch.js

---
+++ 8kyuFlickSwitch.js
@@ -0,0 +1,10 @@
+// Create a function that always returns True/true for every item in a given list.
+// However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+// "flick" will always be given in lowercase.
+// A list may contain multiple flicks.
+// Switch the boolean value on the same element as the flick itself.
+
+function flickSwitch(arr) {
+    let flick = true;
+    return arr.map(item => (item === 'flick' ? flick = !flick : flick));
+  }
