57f81580b5bb3d60ebe410fc5fcd5abe549c98cc
Saraha <sah110_@outlook.com>
2024-02-29 20:12:54 -0500

    Create 8kyuReplaceAllDots.js

---
+++ 8kyuReplaceAllDots.js
@@ -0,0 +1,11 @@
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+// But it's not working properly. Fix the bug.
+
+// var replaceDots = function(str) {
+//     return str.replace(/./, '-');
+//   }
+
+const replaceDots = function (str) {
+    return str.replaceAll(".", "-");
+  };
+
