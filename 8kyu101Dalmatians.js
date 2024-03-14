faf2e84e1489f84621aeb08bc45402bb26de81f3
Saraha <sah110_@outlook.com>
2024-03-13 21:34:44 -0400

    Update 8kyu101Dalmatians.js

--- 8kyu101Dalmatians.js
+++ 8kyu101Dalmatians.js
@@ -19,4 +19,6 @@ function howManyDalmatians(number) {
            dogs[2];
   }
   
+  //or
   
+const howManyDalmatians = number => number <= 10 ? `Hardly any` : number <= 50 ? `More than a handful!` : number === 101 ? `101 DALMATIANS!!!` : `Woah that's a lot of dogs!`;
