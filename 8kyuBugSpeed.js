70072dda87ad9521972fe71a72d59c48dc65606b
Saraha <sah110_@outlook.com>
2024-03-27 20:57:56 -0400

    Create 8kyuBugSpeed.js

---
+++ 8kyuBugSpeed.js
@@ -0,0 +1,5 @@
+// Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored). Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+function bugSpeed(s) {
+    return Math.floor(s * 27.7778);
+   }
