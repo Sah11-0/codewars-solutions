54771c13b7f07bd155ef10b37b1c3452312ee119
Saraha <sah110_@outlook.com>
2024-04-04 23:37:34 -0400

    Create 8kyuExclamationMarks.js

---
+++ 8kyuExclamationMarks.js
@@ -0,0 +1,11 @@
+//Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+function replace(s){
+    let vowels = ['a','e','i','o','u','A','E','I','O','U'];
+      for (let i = 0; i < s.length; i++) {
+        if (vowels.includes(s[i])) {
+         s = s.replace(s[i], '!');
+        }
+      }
+      return s;
+    }
