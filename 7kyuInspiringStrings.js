40087985eed031c6e15a5742f30ca1601ca316d1
Saraha <sah110_@outlook.com>
2024-05-01 22:09:22 -0400

    Create 7kyuInspiringStrings.js

---
+++ 7kyuInspiringStrings.js
@@ -0,0 +1,20 @@
+//Given a string of space separated words, return the longest word.
+//If there are multiple longest words, return the rightmost longest word.
+
+function longestWord(stringOfWords) {
+    const words = stringOfWords.split(" ");
+    let longest = "";
+    let maxLength = 0;
+    
+    for (let i = 0; i < words.length; i++) {
+        const currentWord = words[i];
+        if (currentWord.length >= maxLength) {
+            longest = currentWord;
+            maxLength = currentWord.length;
+        }
+    }
+    
+    return longest;
+}
+
+ 
