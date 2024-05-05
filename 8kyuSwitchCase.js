e683108193e49cf6fb99ca177368f5716fcb0452
Saraha <sah110_@outlook.com>
2024-05-05 10:34:47 -0400

    Update 8kyuSwitchCase.js

--- 8kyuSwitchCase.js
+++ 8kyuSwitchCase.js
@@ -29,3 +29,16 @@ function evalObject(value){
     }
     return result;
   }
+
+  //or
+
+  function evalObject(value){
+    switch(value.operation){
+      case'+': return value.a + value.b;
+      case'-': return value.a - value.b;
+      case'/': return value.a / value.b;
+      case'*': return value.a * value.b;
+      case'%': return value.a % value.b;
+      case'^': return Math.pow(value.a, value.b);
+    }
+  }
