2ceffe7e06b553f1d5ebc602ed2a7b92dacf5872
Saraha <sah110_@outlook.com>
2024-05-09 20:02:42 -0400

    Update 7kyuSumOfNumbers.js

--- 7kyuSumOfNumbers.js
+++ 7kyuSumOfNumbers.js
@@ -14,3 +14,11 @@ function getSum(a, b){
     }
     return sum;
 }
+
+//or
+
+const GetSum = (a, b) => {
+    let min = Math.min(a, b),
+        max = Math.max(a, b);
+    return (max - min + 1) * (min + max) / 2;
+  }
