b3ba0a5a5528f9e03fcaa89ed87ad64ab0e174df
Saraha <sah110_@outlook.com>
2024-05-11 22:15:15 -0400

    Update 8kyuMultipleOfIndex.js

--- 8kyuMultipleOfIndex.js
+++ 8kyuMultipleOfIndex.js
@@ -1,5 +1,7 @@
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 
 function multipleOfIndex(array) {
-    return array.filter((value, index) => value == 0 || value % index === 0);
+    return array.filter((element, index) => value == 0 || element % index === 0);
   }
+
+  const multipleOfIndex = array => array.filter((value, index) => value == 0 || value % index === 0);
