ff967066addee7f78667367bf6be739de19c106f
Saraha <sah110_@outlook.com>
2024-06-26 17:34:38 -0400

    Create 7kyuMostlikely.js

---
+++ 7kyuMostlikely.js
@@ -0,0 +1,10 @@
+// Create a function which compares two probabilities, returning true if the first one is most likely otherwise false.
+// For this exercise probability is expressed as two numbers separated by a colon e.g. a probability of 1 in 3 will be 1:3.
+
+function mostLikely(prob1,prob2){
+    const [a, b] = prob1.split(':').map(Number);
+    const [c, d] = prob2.split(':').map(Number);
+    const p1 = a / (a + b);
+    const p2 = c / (c + d);
+    return p1 > p2;
+  }
