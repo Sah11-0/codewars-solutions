9dcb7d6d80d4c6942f7db4e0acf3d7a0ddfb657f
Saraha <sah110_@outlook.com>
2024-07-16 18:49:27 -0400

    Create 7kyuSortTheGiftCode.js

---
+++ 7kyuSortTheGiftCode.js
@@ -0,0 +1,6 @@
+//Sort the Gift Code
+// Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+function sortGiftCode(code){
+    code.split('').sort().join('')
+  }
