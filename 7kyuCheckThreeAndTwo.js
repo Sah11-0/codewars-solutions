1f22e3ee5ef7c4af046c645758ae6224f8bdeb21
Saraha <sah110_@outlook.com>
2024-07-21 12:26:33 -0400

    Update 7kyuCheckThreeAndTwo.js

--- 7kyuCheckThreeAndTwo.js
+++ 7kyuCheckThreeAndTwo.js
@@ -25,3 +25,17 @@ function checkThreeAndTwo(array) {
      return false;
     }
   }
+
+//solution2 
+function checkThreeAndTwo(array) {
+  const counts = { a: 0, b: 0, c: 0 };
+  
+  for (let char of array) {
+    if (counts.hasOwnProperty(char)) {
+      counts[char]++;
+    }
+  }
+  
+  const values = Object.values(counts);
+  return (values.includes(3) && values.includes(2));
+}
