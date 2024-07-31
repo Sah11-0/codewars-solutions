aee7d801495c74f76c9652cd0ed5df288435ff71
Saraha <sah110_@outlook.com>
2024-07-30 21:19:45 -0400

    Create 7kyuFixMethod.js

---
+++ 7kyuFixMethod.js
@@ -0,0 +1,13 @@
+// Property objMethod should be called by myFunction.Fix the syntax so myFunction will be working again? Please check things like braces, commas, and letter case.
+// IMPORTANT: Returning just "string" is insufficient. We are required to use Object Literal Notation.
+
+function myFunction() {
+    var myObject = {
+      objProperty: "string",
+      objMethod: function() {
+        return myObject.objProperty;
+          }
+    }
+    
+    return myObject.objMethod();
+  };
