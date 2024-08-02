0c562f800dce0ce3d71aacaa8092f77c240204bf
Saraha <sah110_@outlook.com>
2024-08-01 21:08:59 -0400

    Update 7kyuMinimumSteps.js

--- 7kyuMinimumSteps.js
+++ 7kyuMinimumSteps.js
@@ -15,4 +15,14 @@ function minimumSteps(numbers, value) {
     }
   }
   
+//solution 2
+function minimumSteps(numbers, value) {
+  const nums = numbers.slice().sort((a, b) => a - b);
+  for (let i = 0, sum = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (sum >= value) {
+      return i;
+    }
+  }
+}
   
