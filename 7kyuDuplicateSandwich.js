1dc537616148a48edf37479df1da1245b6b88b3c
Saraha <sah110_@outlook.com>
2024-08-11 22:05:59 -0400

    Create 7kyuDuplicateSandwich.js

---
+++ 7kyuDuplicateSandwich.js
@@ -0,0 +1,11 @@
+//In this kata you will be given a list consisting of unique elements except for one thing that appears twice. Your task is to output a list of everything inbetween both occurrences of this element in the list.
+
+function duplicateSandwich(a) {
+    for (let i = 0; i < a.length; i++) {
+      const firstIndex = i;
+      const secondIndex = a.indexOf(a[i], i + 1);
+      if (secondIndex !== -1) {
+        return a.slice(firstIndex + 1, secondIndex);
+      }
+    }
+  }
