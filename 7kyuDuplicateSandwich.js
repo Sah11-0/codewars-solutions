02ebfa910499e90dc785116270815676af0dae19
Saraha <sah110_@outlook.com>
2024-08-12 14:03:32 -0400

    Update 7kyuDuplicateSandwich.js

--- 7kyuDuplicateSandwich.js
+++ 7kyuDuplicateSandwich.js
@@ -9,3 +9,10 @@ function duplicateSandwich(a) {
       }
     }
   }
+
+//solution 2 
+function duplicateSandwich(a) {
+  for (let i = 0; i < a.length - 1; i++)
+    if (a.indexOf(a[i]) !== a.lastIndexOf(a[i]))
+      return a.slice(a.indexOf(a[i]) + 1, a.lastIndexOf(a[i]));
+}
