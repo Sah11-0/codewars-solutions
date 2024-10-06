2ebd1651ed5b24ea53a1fd3b05d86b5e5caa5a33
Saraha <sah110_@outlook.com>
2024-10-05 22:56:29 -0400

    Create 7kyuDroneFlyBy.js

---
+++ 7kyuDroneFlyBy.js
@@ -0,0 +1,5 @@
+// ou will be given two strings: lamps and drone. lamps represents a row of lamps, currently off, each represented by x. When these lamps are on, they should be represented by o.
+// The drone string represents the position of the drone T and its flight path up until this point =. The drone always flies left to right, and always begins at the start of the row of lamps. Anywhere the drone has flown, including its current position, will result in the lamp at that position switching on.
+// Return the resulting lamps string. See example tests for more clarity.
+
+const flyBy = (lamps, drone) => [...lamps].fill(`o`, 0, drone.length).join(``)
