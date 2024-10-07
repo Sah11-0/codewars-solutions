cdd5458de772b4708b038267a933deb0450f49f6
Saraha <sah110_@outlook.com>
2024-10-06 22:52:48 -0400

    Create 8kyuClassicHelloWorld.js

---
+++ 8kyuClassicHelloWorld.js
@@ -0,0 +1,7 @@
+//You are given a method called main, make it print the line Hello World!, and don't return anything.
+
+class Solution {
+    static main() {
+        console.log("Hello World!");
+    }
+}
